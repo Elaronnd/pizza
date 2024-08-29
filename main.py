399ef60c86c15891c7fb8b3c824d4f360dcf1b59
Elaronnd <elaronnd@gmail.com>
2024-08-29 17:30:35 +0300

    added flask and trying do async sqlite and flask

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-from app.database.sqlite_pizza import pizzas_class
+from app.database.sqlite_pizza import sqlite
+import asyncio
+from app.flask_web import web
+
+
+async def main():
+    await web.run(
+        host="localhost",
+        port=5000,
+        debug=True,
+    )
+    pizzas_class = await sqlite()
+    if pizzas_class[0] is False:
+        raise "something with sqlite"
+    await pizzas_class[1].create_db()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
